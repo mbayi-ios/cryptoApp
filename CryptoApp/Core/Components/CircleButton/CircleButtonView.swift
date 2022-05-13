46d01eeb16a1b1704bc8e5dc3cf0c29c16e18242
mbayi-ios <ambayi@lunchbox.io>
2022-05-13 15:32:21 +0300

    created custom circle button for both dark mode and light mode

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleButtonView.swift
+//  CryptoApp
+//
+//  Created by Amby on 13/05/2022.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Image(systemName: "heart.fill")
+            .font(.headline)
+            .foregroundColor(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+            Circle()
+                .foregroundColor(Color.theme.background)
+            )
+            .shadow(color: Color.theme.accent.opacity(0.25), radius: 10, x: 0, y: 0)
+    }
+}
+
+struct CircleButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+        CircleButtonView()
+            .previewLayout(.sizeThatFits)
+
+            CircleButtonView()
+                .previewLayout(.sizeThatFits)
+                .colorScheme(.dark)
+
+        }
+    }
+}
