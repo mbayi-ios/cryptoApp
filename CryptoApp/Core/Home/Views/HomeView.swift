5db7a5f974d49d42c932ed1dcb72bd3c2e1ad37e
mbayi-ios <ambayi@lunchbox.io>
2022-05-13 17:19:35 +0300

    toggle chevron button onTapGesture

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showPortfolio: Bool = false
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
 
             VStack {
-                Text("Header")
+                HStack {
+                    CircleButtonView(iconName: "info")
+                    Spacer()
+                    Text("Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+
                 Spacer(minLength: 0)
             }
         }
