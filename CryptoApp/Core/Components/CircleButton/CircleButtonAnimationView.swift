72310383cd822d46d5d7e4f3c0f897434be2d26a
mbayi-ios <ambayi@lunchbox.io>
2022-05-13 18:21:22 +0300

    create circle to animate

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,33 @@
+//
+//  CircleButtonAnimationView.swift
+//  CryptoApp
+//
+//  Created by Amby on 13/05/2022.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+
+    @State private var animate: Bool = false
+
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none)
+            .onAppear {
+                animate.toggle()
+            }
+
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView()
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
+    }
+}
