3be46c32d92ac54a62df20b265dc0502097b0228
mbayi-ios <ambayi@lunchbox.io>
2022-05-30 16:18:43 +0300

    update on currentHoldings column
    update on currentHoldings column

--- CryptoApp/Core/Home/Views/CoinRowView.swift
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CoinRowView: View {
     let coin: CoinModel
+    let showHoldingsColumn: Bool
 
     var body: some View {
         HStack {
@@ -23,6 +24,15 @@ struct CoinRowView: View {
                 .padding(.leading, 6)
                 .foregroundColor(Color.theme.accent)
             Spacer()
+
+            if showHoldingsColumn {
+                VStack(alignment: .trailing) {
+                    Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                    Text((coin.currentHoldings ?? 0).asNumberString())
+                }
+                .foregroundColor(Color.theme.accent)
+            }
+
             VStack(alignment: .trailing) {
                 Text(coin.currentPrice.asCurrencyWith6Decimals())
                     .bold()
@@ -32,12 +42,14 @@ struct CoinRowView: View {
                         (coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red
                         )
             }
+            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
         }
+        .font(.subheadline)
     }
 }
 
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView(coin: dev.coin)
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
     }
 }
--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -8,6 +8,31 @@
 import Foundation
 
 extension Double {
+    /// convert a double into a currency with 2 decimal places
+    /// ```
+    /// convert 1234.56 to $1,234.56
+
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        //formatter.locale = .current // <- default value
+        //formatter.currencyCode = "usd" // <- Change currency
+        //formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+
+    /// convert a double into a currency as a String with 2 decimal places
+    /// ```
+    /// convert 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
 
     /// convert a double into a currency with 2-6 decimal places
     /// ```
