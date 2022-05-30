d1c3bc05fe0527d3ca69e31db242b6dbfc3315e7
mbayi-ios <ambayi@lunchbox.io>
2022-05-30 17:57:22 +0300

    creating a service to fetch data

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,6 +12,8 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
 
+    
+
     init(){
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             self.allCoins.append(DeveloperPreview.instance.coin)
