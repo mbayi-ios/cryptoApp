4f55dfef193474a86f2451ccbd5aa779ddef7fd9
mbayi-ios <ambayi@lunchbox.io>
2022-05-30 17:04:53 +0300

    added portfolio list coins

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
 
     var body: some View {
@@ -18,6 +18,16 @@ struct HomeView: View {
 
             VStack {
                 homeHeader
+
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
+                }
+
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
 
                 Spacer(minLength: 0)
             }
@@ -31,6 +41,7 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+        .environmentObject(dev.homeVM)
     }
 }
 
@@ -59,5 +70,26 @@ extension HomeView {
                 }
         }
         .padding(.horizontal)
+    }
+
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+
+
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
     }
 }
