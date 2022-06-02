cd2cd3b69b6d4d4066fd360a29a7eb6c9a8ab247
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-02 16:47:36 +0300

    able to pull coin data to the showPortfolioView

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct PortfolioView: View {
 
     @Environment(\.presentationMode) var presentationMode
+
+    @EnvironmentObject private var vm: HomeViewModel
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Hi")
+                    SearchBarView(searchText: $vm.searchText)
+                    ScrollView(.horizontal, showsIndicators: true, content: {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                Text(coin.symbol.uppercased())
+                            }
+                        }
+                    })
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -31,5 +40,6 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }
