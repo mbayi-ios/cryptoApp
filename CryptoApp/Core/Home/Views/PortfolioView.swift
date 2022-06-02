05aea0416cfa448d674a2596553a45f513a7a650
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-02 19:37:46 +0300

    diplaying coins on the portfolioView

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -21,7 +21,7 @@ struct PortfolioView: View {
                     ScrollView(.horizontal, showsIndicators: true, content: {
                         LazyHStack(spacing: 10) {
                             ForEach(vm.allCoins) { coin in
-                                Text(coin.symbol.uppercased())
+                                CoinLogoView(coin: coin)
                             }
                         }
                     })
