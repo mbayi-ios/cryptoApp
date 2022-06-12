5bc36d07ce2ebadc72f8844f2aaa2aeea928ee26
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-12 13:32:56 +0300

    updated portfolio value with the total sum of portfolio coins

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -40,14 +40,6 @@ class HomeViewModel: ObservableObject {
             }
             .store(in: &cancellables)
 
-        //updates market data
-        marketDataService.$marketData
-            .map(mapGlobalMarketData)
-            .sink { [weak self] (returnedStats) in
-                self?.statistics = returnedStats
-            }
-            .store(in: &cancellables)
-
         //updates portfolio coins
         $allCoins
             .combineLatest(portfolioDataService.$savedEntities)
@@ -62,6 +54,15 @@ class HomeViewModel: ObservableObject {
             }
             .sink { [weak self] (returnedCoins) in
                 self?.portfolioCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+
+        //updates market data
+        marketDataService.$marketData
+            .combineLatest($portfolioCoins)
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cancellables)
 
@@ -84,7 +85,7 @@ class HomeViewModel: ObservableObject {
         }
     }
 
-    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?, portfolioCoins: [CoinModel]) -> [StatisticModel] {
         var stats: [StatisticModel] = []
 
         guard let data = marketDataModel else {
@@ -94,7 +95,16 @@ class HomeViewModel: ObservableObject {
         let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
         let volume = StatisticModel(title: "24h Volume", value: data.volume)
         let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
-        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
+
+        let portfolioValue =
+            portfolioCoins
+                .map({$0.currentHoldingsValue})
+                .reduce(0, +)
+
+        let portfolio = StatisticModel(
+            title: "Portfolio Value",
+            value: portfolioValue.asCurrencyWith2Decimals(),
+            percentageChange: 0)
 
         stats.append(contentsOf: [
             marketCap,
