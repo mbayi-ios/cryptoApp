e04f5fe666c11d9b26d608379b6bc85866d2a4f3
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-15 11:06:09 +0300

    update on detail View
    update on detail View

--- CryptoApp/Core/Detail/Views/DetailView.swift
+++ CryptoApp/Core/Detail/Views/DetailView.swift
@@ -22,8 +22,12 @@ struct DetailLoadingView: View {
 }
 
 struct DetailView: View {
-    @StateObject var vm: DetailViewModel
-
+    @StateObject private var vm: DetailViewModel
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30
 
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
@@ -31,12 +35,59 @@ struct DetailView: View {
     }
 
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("")
+                    .frame(height: 150)
+                Text("OverView")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .center,
+                    spacing: spacing,
+                    pinnedViews: [],
+                    content: {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat:  StatisticModel(title:"title", value: "Value"))
+
+                        }
+                    })
+
+                Text("Additional Details")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+
+                LazyVGrid(
+                    columns: columns,
+                    alignment: .center,
+                    spacing: spacing,
+                    pinnedViews: [],
+                    content: {
+                        ForEach(0..<6) { _ in
+                            StatisticView(stat:  StatisticModel(title:"title", value: "Value"))
+
+                        }
+                    })
+            }
+            .padding()
+
+        }
+        .navigationTitle(vm.coin.name)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(coin: dev.coin)
+        NavigationView {
+            DetailView(coin: dev.coin)
+        }
     }
 }
