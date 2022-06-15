2851d2383275367cd9494836a2a8d70f064219a0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-15 09:08:37 +0300

    able to fetch coin details using the subscribers
    able to fetch coin details using the subscribers

--- CryptoApp/Core/Detail/ViewModels/DetailViewModel.swift
+++ CryptoApp/Core/Detail/ViewModels/DetailViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Combine
+
+class DetailViewModel: ObservableObject {
+    private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
+
+    init(coin: CoinModel){
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+
+    private func addSubscribers(){
+        coinDetailService.$coinDetails
+            .sink { (returnedCoinDetails) in
+                print("RECEIVED COIN DATA")
+                print(returnedCoinDetails)
+            }
+            .store(in: &cancellables)
+    }
+}
