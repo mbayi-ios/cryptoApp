3698d0a187f1267fe1975a4afbe32f5b0d3ce025
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-19 18:26:26 +0300

    created counter to loop the animation

--- CryptoApp/Core/Launch/Views/LaunchView.swift
+++ CryptoApp/Core/Launch/Views/LaunchView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct LaunchView: View {
 
-    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var loadingText: [String] = "Loading your portfolio...".map {String($0)}
     @State private var showLoadingText: Bool = false
+    private let timer  = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
 
+    @State private var counter: Int = 0
+    
 
     var body: some View {
         ZStack {
@@ -25,19 +28,41 @@ struct LaunchView: View {
             ZStack {
 
                 if showLoadingText {
-                    Text(loadingText)
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.launch.accent)
-                        .transition(AnyTransition.scale.animation(.easeIn))
+                   /* Text(loadingText)
+
+                        */
+
+                    HStack(spacing: 0) {
+                        ForEach(loadingText.indices) { index in
+                            Text(loadingText[index])
+                                .font(.headline)
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color.launch.accent)
+                                .offset(y: counter == index ? -5 : 0)
+                        }
+                    }
+                    .transition(AnyTransition.scale.animation(.easeIn))
                 }
             }
             .offset(y: 70)
         }
         .onAppear{
             showLoadingText.toggle()
-            
+
         }
+        .onReceive(timer, perform: { _ in
+            withAnimation(.spring()) {
+
+                let lastIndex = loadingText.count - 1
+
+                if counter == lastIndex {
+                    counter = 0
+                } else {
+                    counter += 1
+                }
+
+            }
+        })
     }
 }
 
