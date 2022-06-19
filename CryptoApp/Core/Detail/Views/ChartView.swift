b3a5337a37f2ce6af9dba3c46d4dc853490e4107
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-19 12:39:01 +0300

    changed the chart color

--- CryptoApp/Core/Detail/Views/ChartView.swift
+++ CryptoApp/Core/Detail/Views/ChartView.swift
@@ -13,11 +13,17 @@ struct ChartView: View {
     let maxY: Double
     let minY: Double
 
+    let lineColor: Color
+
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
         maxY = data.max() ?? 0
         minY = data.min() ?? 0
+
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
     }
+
 
     var body: some View {
         GeometryReader { geometry in
@@ -38,7 +44,7 @@ struct ChartView: View {
                 }
 
             }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
         }
     }
 }
