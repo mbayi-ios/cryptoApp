8544e497047ae288b0a988e22278a78a5febe9c2
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-06-19 12:28:29 +0300

    creating a chart image

---
+++ CryptoApp/Core/Detail/Views/ChartView.swift
@@ -0,0 +1,40 @@
+//
+//  ChartView.swift
+//  CryptoApp
+//
+//  Created by Amby on 19/06/2022.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+
+    let data: [Double]
+    init(coin: CoinModel) {
+        data = coin.sparklineIn7D?.price ?? []
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+
+                    if index == 0 {
+                        path.move(to: CGPoint(x: 0, y: 0))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: 0))
+                }
+
+            }
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        }
+    }
+}
+
+struct Chart_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView(coin: dev.coin)
+            .frame(width: 200)
+    }
+}
